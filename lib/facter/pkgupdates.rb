a9d5d9fc5055f48e6f2ef10a03cb1407be16c248
Cody Herriges <cody@herriges.org>
2015-01-30 17:04:44 -0800

    Add Solaris 11 support.
      Commit will add initial Solaris 11 support using pkg.

--- lib/facter/pkgupdates.rb
+++ lib/facter/pkgupdates.rb
@@ -6,7 +6,8 @@ require 'facter/util/pkgupdates'
 updates = Facter::Util::Pkgupdates.get_packages()
 
 Facter.add(:pkg_updates) do
-  confine :osfamily => %w{Debian RedHat}
+  confine :osfamily => %w{Debian RedHat Solaris}
+  confine :operatingsystemrelease => '5.11' if Facter.value(:osfamily) == 'Solaris'
   setcode do
     updates
   end
--- lib/facter/util/pkgupdates.rb
+++ lib/facter/util/pkgupdates.rb
@@ -21,6 +21,15 @@ module Facter::Util::Pkgupdates
         updates[list[0]]['current'] = Facter::Util::Resolution.exec("rpm --query --qf '%{VERSION}-%{RELEASE}' #{list[0]}")
         updates[list[0]]['update'] = list[1].split(':')[-1]
       end
+    when 'Solaris'
+      command = 'pkg list -u -H'
+      lines = Facter::Util::Resolution.exec(command).split("\n")
+      lines.each do |pkg|
+        list = pkg.split(/ +/)
+        updates[list[0]] = {}
+        updates[list[0]]['current'] = list[1]
+        updates[list[0]]['update'] = Facter::Util::Resolution.exec("pkg info -r #{list[0]}").scan(/Version: (.*)/)[0][0]
+      end
     end
     return updates
   end
